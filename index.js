fcd7b6ddf50f5ca2e37bdaed0a89df1857411965
Theodore P <evil69669@gmail.com>
2016-05-28 16:18:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.get('/', function (req, res) {
 
-app.get('/', function(req, res) {
-     res.json({
+  res.json({
         ipaddress: req.headers['x-forwarded-for'] ||
                    req.connection.remoteAddress,
         language: req.headers["accept-language"].split(",")[0],
         software: req.headers['user-agent'].split(') ')[0].split(' (')[1]
     });
   
+
+   
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("App listening at http://%s:%s", host, port)
+
+})
